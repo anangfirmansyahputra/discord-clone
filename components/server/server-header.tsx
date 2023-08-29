16221f748c07a67e9c8215ba714a8e5f291b778e
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-29 19:46:01 +0700

    delete & leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -67,13 +67,19 @@ const ServerHeader = ({ server, role }: Props) => {
 				)}
 				{isModerator && <DropdownMenuSeparator />}
 				{isAdmin && (
-					<DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+					<DropdownMenuItem
+						onClick={() => onOpen('deleteServer', { server })}
+						className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+					>
 						Delete Server
 						<TrashIcon className='w-4 h-4 ml-auto' />
 					</DropdownMenuItem>
 				)}
 				{!isAdmin && (
-					<DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+					<DropdownMenuItem
+						onClick={() => onOpen('leaveServer', { server })}
+						className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+					>
 						Leave Server
 						<LogOut className='w-4 h-4 ml-auto' />
 					</DropdownMenuItem>
