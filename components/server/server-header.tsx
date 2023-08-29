3f5fbc63c39504061f6741d96d7c082dac37d6b4
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-29 18:07:09 +0700

    manage members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -48,7 +48,10 @@ const ServerHeader = ({ server, role }: Props) => {
 					</DropdownMenuItem>
 				)}
 				{isAdmin && (
-					<DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+					<DropdownMenuItem
+						onClick={() => onOpen('members', { server })}
+						className='px-3 py-2 text-sm cursor-pointer'
+					>
 						Manage Members
 						<Users className='w-4 h-4 ml-auto' />
 					</DropdownMenuItem>
