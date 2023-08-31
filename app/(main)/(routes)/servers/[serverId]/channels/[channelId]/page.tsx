36eccf95a3f9352c9997e2d343a404b05eb43b48
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-31 08:36:48 +0700

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,3 +1,43 @@
-export default function Page() {
-	return <div>Page</div>;
+import ChatHeader from '@/components/chat/chat-header';
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+interface Props {
+	params: {
+		serverId: string;
+		channelId: string;
+	};
+}
+
+export default async function Page({ params }: Props) {
+	const profile = await currentProfile();
+
+	if (!profile) return redirectToSignIn();
+
+	const channel = await db.channel.findUnique({
+		where: {
+			id: params.channelId,
+		},
+	});
+
+	const member = await db.member.findFirst({
+		where: {
+			serverId: params.serverId,
+			profileId: profile.id,
+		},
+	});
+
+	if (!channel || !member) return redirect('/');
+
+	return (
+		<div className='bg-white dark:bg-[#313338] flex flex-col w-full'>
+			<ChatHeader
+				name={channel.name}
+				type={'channel'}
+				serverId={channel.serverId}
+			/>
+		</div>
+	);
 }
