d94a5b1c06dc26a4f59142d29ec017eb3ffc4dc5
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-31 12:18:08 +0700

    conversation setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from 'lucide-react';
 import MobileToggle from '../mobile-toggle';
+import UserAvatar from '../user-avatar';
 
 interface Props {
 	serverId: string;
@@ -13,6 +14,12 @@ const ChatHeader = ({ name, serverId, type, imageUrl }: Props) => {
 		<div className='text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2'>
 			<MobileToggle serverId={serverId} />
 			{type === 'channel' && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
+			{type === 'conversation' && (
+				<UserAvatar
+					src={imageUrl}
+					className='h-8 w-8 mr-2 md:w-8 md:h-8'
+				/>
+			)}
 			<p className='font-semibold text-md text-black dark:text-white'>{name}</p>
 		</div>
 	);
